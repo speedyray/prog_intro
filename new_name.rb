a70a76e9c76ca93895d6e4a9e6a2eab43f97e2a4
speedyray <speedyray2ray@gmail.com>
2014-11-22 19:55:26 -0500

    solution for variables exercise 4

---
+++ new_name.rb
@@ -0,0 +1,12 @@
+
+  
+      puts "Please type in your first name"
+  
+      first_name = gets.chomp
+      
+      puts "Please type in your last name"
+      last_name = gets.chomp
+      
+   
+      puts  "Welcome " +  first_name +  " " + last_name
+     
