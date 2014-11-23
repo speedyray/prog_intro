2d5b49afa50f7a70943baa21fa2ed3e53ba574f3
speedyray <speedyray2ray@gmail.com>
2014-11-22 19:08:40 -0500

    Solution for chapter 1 exercise 3

---
+++ my_hash.rb
@@ -0,0 +1,10 @@
+     
+     
+     
+     my_movies = {:boxer => 1975, :true_love => 2004, :kill_bill => 2013, :saturn => 2001, :dogs_of_war => 1981 }
+
+     puts my_movies[:boxer]
+     puts my_movies[:true_love]
+     puts my_movies[:kill_bill]
+     puts my_movies[:saturn]
+     puts my_movies[:dogs_of_war]
