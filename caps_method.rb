ebef9e97fb1eace5e5dfe9fa791c5cb814e9c3d6
speedyray <speedyray2ray@gmail.com>
2014-11-22 20:55:59 -0500

    solution for flow_control exercise 2

---
+++ caps_method.rb
@@ -0,0 +1,14 @@
+
+  def caps_method(name)
+   
+     if name.length > 10
+      name.upcase
+     else
+      name
+     end
+  end
+
+  
+  puts caps_method("santaclaus")
+
+  puts caps_method("shokolokobangoshi")
