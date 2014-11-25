156a0f1b80bc1e56102369395d4a8dcbfb8c8461
speedyray <speedyray2ray@gmail.com>
2014-11-24 21:22:37 -0500

    solution for loops exercise 4

---
+++ count_to_zero_recursion.rb
@@ -0,0 +1,18 @@
+
+def count_to_zero(x)
+
+ 
+ if x <= 0
+ puts x
+ else
+ puts x
+ count_to_zero(x - 1)
+ end
+ 
+end
+
+
+count_to_zero(20)
+count_to_zero(30)
+count_to_zero(56)
+count_to_zero(-5)
