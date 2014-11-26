5e8ab4e44436cd2c4fe359243a7bdf60fe5781be
speedyray <speedyray2ray@gmail.com>
2014-11-25 21:46:55 -0500

    solution for Array exercise 6

---
+++ error_message.rb
@@ -0,0 +1,17 @@
+
+
+
+      names = ['bob', 'joe', 'susan', 'margaret']
+      names['margaret'] = 'jody'
+
+      ...and get the following error message:
+
+      TypeError: no implicit conversion of String into Integer
+        from (irb):2:in `[]='
+        from (irb):2
+        from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+        
+        
+        
+        
+        names[3] = 'jody'
