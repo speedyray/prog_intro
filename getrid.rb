77c0a7f9f237a5f8999b99af102a45e9960b88bd
speedyray <speedyray2ray@gmail.com>
2014-11-29 17:16:30 -0500

    solution for final exercise 5

---
+++ getrid.rb
@@ -0,0 +1,20 @@
+
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+added_eleven = arr.push(11)
+
+# Remove 11 from end of array
+added_eleven.pop
+ 
+p added_eleven
+ 
+ 
+# And append a "3"
+added_eleven.push(3)
+ 
+p added_eleven
+
+
+
+
