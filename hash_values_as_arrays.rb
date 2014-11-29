3a03062d4ffec39c966432443ea647152b8e9fac
speedyray <speedyray2ray@gmail.com>
2014-11-29 18:04:16 -0500

    solution for final exercise 10

---
+++ hash_values_as_arrays.rb
@@ -0,0 +1,15 @@
+  
+  
+  
+  
+  # can hash values be arrays? Yes
+  # hash values as arrays
+  # These are hashes made with array values
+  hash = {names: ['bob', 'joe', 'susan'], ages:[24, 30, 28] }
+  p hash
+
+
+  # array of hashes
+  # These are arrays made with hash values
+  arr = [{name: 'bob'}, {name: 'joe'}, {name: 'susan'}]
+  p arr
